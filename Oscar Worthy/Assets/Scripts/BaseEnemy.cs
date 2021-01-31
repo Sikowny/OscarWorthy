e97d7435cb74eefb460cdc4f24c4a016e34c70c2
Hilda <eskarina@eskarina.ca>
2021-01-31 00:35:38 -0800

    Made enemies only react to player when nearby

--- Oscar Worthy/Assets/Scripts/BaseEnemy.cs
+++ Oscar Worthy/Assets/Scripts/BaseEnemy.cs
@@ -17,6 +17,8 @@ public class BaseEnemy : MonoBehaviour
     float jumpSpeed = 0.01f;
     float gravity = -0.0001f;
 
+    float playerDetectionRange = 15.0f; // How close the player must be for the enemy to pursue them
+
     float attackRange = 2.25f;
     float attackWindUp = 1.0f;      // time the enemy must stand still before attacking
     float attackWindDown = 0.5f;    // time the enemy stands still after an attack
@@ -42,7 +44,8 @@ public class BaseEnemy : MonoBehaviour
         if (currAttackState == AttackState.OutOfRange)
         {
             HandleHorizontalMovement();
-        } else
+        }
+        else
         {
             HandleAttack();
         }
@@ -52,29 +55,60 @@ public class BaseEnemy : MonoBehaviour
 
     void HandleHorizontalMovement()
     {
-        // Rotate to look at the player
-        // To ensure the enemy remains upright, the enemy's own y-position is used when determining where to look
-        Vector3 lookTarget = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
-        transform.LookAt(lookTarget);
-
         Vector3 vectorToPlayer = player.transform.position - this.transform.position;
-        if (vectorToPlayer.magnitude <= attackRange)
+
+        if (vectorToPlayer.magnitude <= playerDetectionRange)
         {
-            currAttackState = AttackState.WindingUp;
-            attackTimer = attackWindUp;
-            attackDirection = (player.transform.position - this.transform.position).normalized;
-
-            velocity = new Vector3(0, 0, 0);
+            // Rotate to look at the player
+            // To ensure the enemy remains upright, the enemy's own y-position is used when determining where to look
+            Vector3 lookTarget = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+            transform.LookAt(lookTarget);
         }
-        else {
-            Vector3 acceleration = vectorToPlayer.normalized * accelerationRate * Time.deltaTime;
-            acceleration.y = 0;
 
-            velocity += acceleration;
-            if (velocity.magnitude > speed)
+        // check if the enemy is over solid ground
+        RaycastHit hit;
+        Vector3 rayOffset = transform.position + transform.TransformDirection(Vector3.forward) * 0.2f + new Vector3(0, 1, 0);
+        if (Physics.Raycast(rayOffset, transform.TransformDirection(Vector3.down), out hit))
+        {
+            // solid ground - can move
+            // Note that the enemy will keep moving as long as it detects solid ground below it, even if it's lower, so it can go off ledges
+            // Gravity isn't implemented yet
+            if (vectorToPlayer.magnitude <= attackRange)
             {
-                velocity = velocity.normalized * speed;
+                currAttackState = AttackState.WindingUp;
+                attackTimer = attackWindUp;
+                attackDirection = (player.transform.position - this.transform.position).normalized;
+
+                velocity = new Vector3(0, 0, 0);
             }
+            else if (vectorToPlayer.magnitude <= playerDetectionRange)
+            {
+                Vector3 acceleration = vectorToPlayer.normalized * accelerationRate * Time.deltaTime;
+                acceleration.y = 0;
+
+                velocity += acceleration;
+                if (velocity.magnitude > speed)
+                {
+                    velocity = velocity.normalized * speed;
+                }
+            }
+            else
+            {
+                // If the player is out of range, the enemy decelerates and comes to a rest
+                if (velocity.magnitude > 0.001f)
+                {
+                    velocity -= velocity * 2.0f * Time.deltaTime;
+                }
+                else
+                {
+                    velocity = new Vector3(0, 0, 0);
+                }
+            }
+        }
+        else
+        {
+            // no solid ground; stop moving
+            velocity = new Vector3(0, 0, 0);
         }
     }
 
@@ -98,20 +132,23 @@ public class BaseEnemy : MonoBehaviour
 
     void HandleAttack()
     {
-        if (currAttackState == AttackState.WindingUp) {
+        if (currAttackState == AttackState.WindingUp)
+        {
             attackTimer -= Time.deltaTime;
-            if(attackTimer <= 0)
+            if (attackTimer <= 0)
             {
                 currAttackState = AttackState.Attacking;
             }
-        } else if(currAttackState == AttackState.Attacking)
+        }
+        else if (currAttackState == AttackState.Attacking)
         {
             Vector3 attackHitboxPos = transform.position + (attackDirection * attackRange * 0.5f) + new Vector3(0, 1.25f, 0);
             Instantiate(attackHitboxPrefab, attackHitboxPos, transform.rotation);
 
             attackTimer = attackWindDown;
             currAttackState = AttackState.WindingDown;
-        } else if(currAttackState == AttackState.WindingDown)
+        }
+        else if (currAttackState == AttackState.WindingDown)
         {
             attackTimer -= Time.deltaTime;
             if (attackTimer <= 0)
